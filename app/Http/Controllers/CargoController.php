da0d4e847e78f2389378b2b26c9614947f446f17
craken2020 <craken1994@gmail.com>
2021-09-16 16:08:15 -0500

    cambio 3

--- app/Http/Controllers/CargoController.php
+++ app/Http/Controllers/CargoController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Cargo;
 
 class CargoController extends Controller
 {
@@ -13,7 +14,10 @@ class CargoController extends Controller
      */
     public function index()
     {
-        return view('cargos/index');
+       
+        return Cargo::all();
+        //return view('cargos/index');
+   
     }
 
     /**
